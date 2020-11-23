270eb9c795d6253df8f8ff3d7b63cebd4b8d6ebd
Matthew Estes <matthew.k.estes@gmail.com>
2020-11-22 18:42:22 -0800

    Improved efficiency of calculation algorithm

--- script.js
+++ script.js
@@ -14,185 +14,203 @@ const result = document.querySelector("#result");
 var diceList = [];
 
 function removeDie(butRef) {
-  butRef.remove();
+    butRef.remove();
 }
 
 function addDie(sides) {
-  var dieButton = document.createElement("button");
-  var dieNumText = document.createTextNode("d" + sides);
-  dieButton.appendChild(dieNumText);
-  dieButton.addEventListener(
-    "click",
-    function() {
-      removeDie(dieButton);
-      calculate();
-    },
-    false
-  );
-  diceDisplay.appendChild(dieButton);
-  calculate();
+    var dieButton = document.createElement("button");
+    var dieNumText = document.createTextNode("d" + sides);
+    dieButton.appendChild(dieNumText);
+    dieButton.addEventListener(
+        "click",
+        function() {
+            removeDie(dieButton);
+            calculate();
+        },
+        false
+    );
+    diceDisplay.appendChild(dieButton);
+    calculate();
 }
 
 function calculate() {
-  var target = targetText.value;
-  console.log("Target: " + target);
-  if (isNaN(target)) {
-    result.innerHTML = "Target is not a valid number.";
-    return;
-  }
-
-  var mainDice = [];
-  var d4s = [];
-
-  var diceSelected = diceDisplay.children;
-  for (let i = 0; i < diceSelected.length; i++) {
-    let dieSides = diceSelected[i].innerHTML.substring(1);
-    if (dieSides == 4) {
-      d4s.push(dieSides);
-    } else {
-      mainDice.push(dieSides);
-    }
-  }
-  console.log(mainDice);
-  console.log(d4s);
-  if (mainDice.length < 2) {
-    result.innerHTML =
-      "A roll always has at least 2 dice with 6 sides or more.";
-    return;
-  }
-
-  //console.log("starting rolling main dice");
-  var mainDiceSets = rollDice(mainDice);
-  //console.log(mainDiceSets);
-  //console.log("finished rolling main dice");
-  var d4Sets = rollDice(d4s);
-
-  var fullSums = [];
-  var mainDiceTopSums = [];
-
-  for (let mds = 0; mds < mainDiceSets.length; mds++) {
-    let biggest = 0;
-    let secondBiggest = 0;
-    let currentSet = mainDiceSets[mds];
-    for (let r = 0; r < currentSet.length; r++) {
-      if (currentSet[r] > biggest) {
-        secondBiggest = biggest;
-        biggest = currentSet[r];
-      } else if (currentSet[r] > secondBiggest) {
-        secondBiggest = currentSet[r];
-      }
+    var target = targetText.value;
+    console.log("Target: " + target);
+    if (isNaN(target)) {
+        result.innerHTML = "Target is not a valid number.";
+        return;
     }
 
-    mainDiceTopSums.push(biggest + secondBiggest);
-  }
+    var mainDice = [];
+    var d4s = [];
 
-
-  if (d4Sets.length > 0) {
-    let d4SetTops = [];
-    for (let d4s = 0; d4s < d4Sets.length; d4s++) {
-      let biggest = 0;
-      let currentSet = d4Sets[d4s];
-
-      for (let r = 0; r < currentSet.length; r++) {
-        if (currentSet[r] > biggest) {
-          biggest = currentSet[r];
+    var diceSelected = diceDisplay.children;
+    for (let i = 0; i < diceSelected.length; i++) {
+        let dieSides = diceSelected[i].innerHTML.substring(1);
+        if (dieSides == 4) {
+            d4s.push(dieSides);
+        } else {
+            mainDice.push(dieSides);
         }
-      }
-      d4SetTops.push(biggest);
+    }
+    console.log(mainDice);
+    console.log(d4s);
+    if (mainDice.length < 2) {
+        result.innerHTML = "A roll always has at least 2 dice with 6 sides or more.";
+        return;
     }
 
-    for (let i = 0; i < mainDiceTopSums.length; i++) {
-      for (let j = 0; j < d4SetTops.length; j++) {
-        fullSums.push(mainDiceTopSums[i] + d4SetTops[j]);
-      }
-    }
-  } else {
-    fullSums = mainDiceTopSums;
-  }
+    var totalSums = [];
+    var mainDiceSums = mainDiceOutcomes(mainDice);
+    if (d4s.length > 0) {
+        //console.log(d4s);
+        var divineFavorDiceSums = divineFavorDiceOutcomes(d4s);
+        for (var i = 0; i < mainDiceSums.length + divineFavorDiceSums.length; i++) {
+            totalSums.push(0);
+        }
 
-  var successCount = 0;
-  for (let i = 0; i < fullSums.length; i++) {
-    if (fullSums[i] >= target) {
-      successCount++;
+        for (var i = 1; i <= mainDiceSums.length; i++) {
+            for (var j = 1; j <= divineFavorDiceSums.length; j++) {
+                totalSums[i + j - 1] += mainDiceSums[i - 1] * divineFavorDiceSums[j - 1];
+            }
+        }
+    } else {
+        totalSums = mainDiceSums;
     }
-  }
-  var successPercentage = ((100 * successCount) / fullSums.length).toFixed(2);
-  result.innerHTML = "Chance of success: " + successPercentage + "%";
+
+    var totalPossibilities = 0;
+    var winningPossibilities = 0;
+    for (var i = 1; i <= totalSums.length; i++) {
+        totalPossibilities += totalSums[i - 1];
+        if (i >= target) {
+            winningPossibilities += totalSums[i - 1];
+        }
+    }
+    console.log(winningPossibilities);
+    console.log(totalPossibilities);
+    var successPercentage = ((100 * winningPossibilities) / totalPossibilities).toFixed(2);
+    result.innerHTML = "Chance of success: " + successPercentage + "%";
 }
 
-//calculateButton.addEventListener("click", calculate, false);
-
-function rollDice(dice) {
-  var rollSets = [];
-
-  //adds each value of a face of the first die, contained in a single-item list, to rollSets
-  if (dice.length > 0) {
-    for (let r = 1; r <= dice[0]; r++) {
-      rollSets.push([r]);
-    }
-  }
-  //console.log(rollSets);
-
-  if (dice.length > 1) {
-    //console.log("adding additional dice; " + dice.length + " total dice");
-    // for each die in the list of dice, after the first die
-    for (let d = 1; d < dice.length; d++) {
-      let newRollSets = [];
-
-      for (let r = 1; r <= dice[d]; r++) {
-        //console.log("current face value: " + r);
-        //console.log("total roll sets right now: " + rollSets.length);
-        for (let rs = 0; rs < rollSets.length; rs++) {
-          let rollList = rollSets[rs].slice();
-          //console.log("current roll list:" + rollList);
-          rollList.push(r);
-          //console.log("current roll list now:" + rollList);
-          newRollSets.push(rollList);
+function mainDiceOutcomes(diceList) {
+    //var maxSides = Math.max(diceList[0], diceList[1]);
+    var maxSides = 12;
+    var outcomeDistribution = [];
+    for (var i = 0; i < maxSides; i++) {
+        var row = [];
+        for (var j = 0; j < maxSides; j++) {
+            row.push(0);
         }
-      }
-
-      rollSets = newRollSets;
-      //console.log(rollSets);
+        outcomeDistribution.push(row);
     }
-  }
 
-  return rollSets;
+    for (var i = 1; i <= diceList[0]; i++) {
+        for (var j = 1; j <= diceList[1]; j++) {
+            var biggerSide = Math.max(i, j);
+            var smallerSide = Math.min(i, j);
+            outcomeDistribution[biggerSide - 1][smallerSide - 1]++;
+        }
+    }
+
+    for (var d = 2; d < diceList.length; d++) {
+        var newOutcomeDistribution = [];
+        for (var i = 0; i < maxSides; i++) {
+            var row = [];
+            for (var j = 0; j < maxSides; j++) {
+                row.push(0);
+            }
+            newOutcomeDistribution.push(row);
+        }
+
+        for (var i = 1; i <= maxSides; i++) {
+            for (var j = 1; j <= maxSides; j++) {
+                for (var s = 1; s <= diceList[d]; s++) {
+                    var biggestSide = Math.max(i, s);
+                    var secondBiggest = Math.max(Math.min(i, s), j);
+                    newOutcomeDistribution[biggestSide - 1][secondBiggest - 1] += outcomeDistribution[i - 1][j - 1];
+                }
+            }
+        }
+        outcomeDistribution = newOutcomeDistribution;
+    }
+
+    //console.log(outcomeDistribution);
+
+    var sumDistribution = [];
+    for (var i = 0; i < maxSides * 2; i++) {
+        sumDistribution.push(0);
+    }
+    for (var i = 1; i <= maxSides; i++) {
+        for (var j = 1; j <= maxSides; j++) {
+            sumDistribution[i + j - 1] += outcomeDistribution[i - 1][j - 1];
+        }
+    }
+    //console.log(sumDistribution);
+    return sumDistribution;
+}
+
+function divineFavorDiceOutcomes(diceList) {
+    var maxSides = 4;
+    var outcomeDistribution = [];
+    for (var i = 0; i < maxSides; i++) {
+        outcomeDistribution.push(0);
+    }
+
+    for (var i = 1; i <= diceList[0]; i++) {
+        outcomeDistribution[i - 1]++;
+    }
+
+    for (var d = 1; d < diceList.length; d++) {
+        var newOutcomeDistribution = [];
+        for (var i = 0; i < maxSides; i++) {
+            newOutcomeDistribution.push(0);
+        }
+
+        for (var i = 1; i <= outcomeDistribution.length; i++) {
+            for (var s = 1; s <= diceList[d]; s++) {
+                var biggestSize = Math.max(i, s);
+                newOutcomeDistribution[biggestSize - 1] += outcomeDistribution[i - 1];
+            }
+        }
+        outcomeDistribution = newOutcomeDistribution;
+    }
+    return outcomeDistribution;
 }
 
 d4Button.addEventListener(
-  "click",
-  function() {
-    addDie(4);
-  },
-  false
+    "click",
+    function() {
+        addDie(4);
+    },
+    false
 );
 d6Button.addEventListener(
-  "click",
-  function() {
-    addDie(6);
-  },
-  false
+    "click",
+    function() {
+        addDie(6);
+    },
+    false
 );
 d8Button.addEventListener(
-  "click",
-  function() {
-    addDie(8);
-  },
-  false
+    "click",
+    function() {
+        addDie(8);
+    },
+    false
 );
 d10Button.addEventListener(
-  "click",
-  function() {
-    addDie(10);
-  },
-  false
+    "click",
+    function() {
+        addDie(10);
+    },
+    false
 );
 d12Button.addEventListener(
-  "click",
-  function() {
-    addDie(12);
-  },
-  false
+    "click",
+    function() {
+        addDie(12);
+    },
+    false
 );
 
 targetText.addEventListener("keyup", calculate, false);
